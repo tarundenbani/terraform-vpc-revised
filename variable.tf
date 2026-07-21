fdb82646f1fe42f666feb9a75295882c9f0b7234
tarundenbani <36141522+tarundenbani@users.noreply.github.com>
2026-07-21 21:42:49 +0530

    Remove unnecessary variables from variable.tf
    Removed unused variables related to database route table, IGW creation, DHCP options, and public subnets.

--- variable.tf
+++ variable.tf
@@ -1,14 +1,3 @@
-variable "database_route_table_association_required" {
-  description = "Whether db route table association required"
-  type        = bool
-  default     = null
-}
-
-variable "create_igw" {
-  description = "Whether IGW needs to be created"
-  type        = bool
-  default     = null
-}
 variable "igw_tag" {
   description = "Mention Tag needs to be associated with internet gateway"
   type        = string
@@ -40,11 +29,6 @@ variable "enable_dns_support" {
   type        = bool
   default     = null
 }
-variable "enable_ipv6" {
-  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block."
-  type        = bool
-  default     = null
-}
 
 variable "myVpc_tag" {
   description = "Tag Name to be assigned with VPC"
@@ -52,23 +36,6 @@ variable "myVpc_tag" {
   default     = "mt"
 }
 
-variable "default_security_group_name" {
-  description = "Enter the name for security group"
-  type        = string
-  default     = null
-}
-
-variable "enable_dhcp_options" {
-  description = "Enable DHCP options.. True or False"
-  type        = bool
-  default     = null
-}
-
-variable "manage_default_route_table" {
-  description = "Are we managing default route table"
-  type        = bool
-  default     = null
-}
 variable "public_subnet_1_tag" {
   description = "Tag for public subnet"
   type        = string
@@ -78,17 +45,6 @@ variable "public_subnet_2_tag" {
   description = "Tag for public subnet"
   type        = string
   default     = "ps2t"
-}
-variable "database_subnets" {
-  description = "CIDR block for database subnet"
-  type        = list(any)
-  default     = null
-}
-
-variable "public_subnet" {
-  description = "enter the number of public subnets you need"
-  type        = number
-  default     = null
 }
 
 variable "public_subnet_cidr_1" {
